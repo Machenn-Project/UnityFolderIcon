31728c4b1a30c0c0a2d3efa49a79cee454cfded1
PUVIYARASU P <puviyarasu5341@gmail.com>
2024-06-11 13:14:06 +0530

    1st upload

---
+++ Editor/Machenn.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Machenn 
+{
+    // Start is called before the first frame update
+    class MachennClass
+    {
+        public void Main()
+        {
+            Debug.Log("Inside");
+        }
+    }
+
+}
